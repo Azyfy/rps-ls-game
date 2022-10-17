d06fdae45d81378aff0563a41fa91c7fc533efff
Adi <adi92az@gmail.com>
2022-10-17 18:02:28 +0200

    Set config for react hot loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {hot} from "react-hot-loader/root"; // import before react and react-dom
+import { setConfig } from 'react-hot-loader';
 import React from "react";
 import "./App.css"
 
@@ -9,5 +10,13 @@ function App() {
         </div>
     )
 }
+
+setConfig({
+    showReactDomPatchNotification: false
+}) /*  Dont show warning in console:
+React-Hot-Loader: react-🔥-dom patch is not detected. React 16.6+ features may not work.
+
+@hot-loader/react-dom would solve the warning but it replaces react-dom
+*/
 
 export default hot(App);
