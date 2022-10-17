d6b8a0da0fa4d4a12a208554a7420c25534ade96
Adi <adi92az@gmail.com>
2022-10-17 19:42:36 +0200

    Lint

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import {hot} from "react-hot-loader/root"; // import before react and react-dom
-import { setConfig } from 'react-hot-loader';
-import React from "react";
+import { hot } from "react-hot-loader/root" // import before react and react-dom
+import { setConfig } from "react-hot-loader"
+import React from "react"
 import "./App.scss"
 
 function App() {
-    return (
-        <div className="App" >
-            <h1>Invoice App</h1>
-        </div>
-    )
+	return (
+		<div className="App" >
+			<h1>Invoice App</h1>
+		</div>
+	)
 }
 
 setConfig({
-    showReactDomPatchNotification: false
+	showReactDomPatchNotification: false
 }) /*  Dont show warning in console:
 React-Hot-Loader: react-🔥-dom patch is not detected. React 16.6+ features may not work.
 
 @hot-loader/react-dom would solve the warning but it replaces react-dom
 */
 
-export default hot(App);
+export default hot(App)
