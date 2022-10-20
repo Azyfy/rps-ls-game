628e4e76848752f9430fcda0ba7166ddacb63212
Adi <adi92az@gmail.com>
2022-10-20 21:03:17 +0200

    Lint

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -5,18 +5,18 @@ import rpsRules from "../img/rps-rules.svg"
 import rpslsRules from "../img/rpsls-rules.svg"
 
 export default function Rules({ mode, toggelRules }) {
-    return (
-        <div className="👻" >
-            <div className="📜" >
-                <div>
-                    <p> Rules </p>
-                </div>
-                <img src={ (mode == "Original") ? rpsRules : rpslsRules } />
-                <div onClick={ toggelRules } >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div className="👻" >
+			<div className="📜" >
+				<div>
+					<p> Rules </p>
+				</div>
+				<img src={ (mode == "Original") ? rpsRules : rpslsRules } />
+				<div onClick={ toggelRules } >
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+						<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
+				</div>
+			</div>
+		</div>
+	)
 }
