ce1f7378105c72404137ca6942ff04aaa67ccd73
Adi <adi92az@gmail.com>
2022-10-23 18:24:16 +0200

    Style breakpoint

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -5,14 +5,19 @@ import rpsRules from "../img/image-rules.svg"
 import rpslsRules from "../img/image-rules-bonus.svg"
 
 export default function Rules({ mode, toggelRules }) {
+
+	function handleDisabled(e) {
+		e.stopPropagation()
+	}
+
 	return (
-		<div className="👻" >
-			<div className="📜" >
+		<div className="👻" onClick={toggelRules} >
+			<div className="📜" onClick={(e) => handleDisabled(e)} >
 				<div>
 					<p> Rules </p>
 				</div>
 				<img src={ (mode == "Original") ? rpsRules : rpslsRules } alt="Rules" />
-				<div onClick={ toggelRules } >
+				<div onClick={ toggelRules } className="✖️" >
 					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
 						<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
 				</div>
